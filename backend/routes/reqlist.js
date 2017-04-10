7667c5de99522a152531f3db1413b7c207b34956
lmk <newtypez@gmail.com>
2017-04-10 16:17:51 +0900

    backend /list 추가

--- backend/routes/reqlist.js
+++ backend/routes/reqlist.js
@@ -1,10 +1,21 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  
-  res.send('respond with a resource');
+
+  fs.readFile(__dirname + '/../config.json', 'utf-8', (err, data)=>{
+    if (err != null)
+    {
+      console.log('[ERROR] config file read fail: ' + __dirname + '/../config.json', err);
+      return;
+    }
+
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(data);
+  });
+
 });
 
 module.exports = router;
